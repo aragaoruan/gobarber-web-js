59b152b7e87509b02b6b1ec34916b42db705fd6b
Ruan Aragao <aragao.ruan@gmail.com>
2019-10-16 18:17:36 -0300

    [Add] rotas privadas

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,13 @@ module.exports = {
     'no-console': ["error", {allow: ["tron"]}],
 
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
+
+    "react/jsx-props-no-spreading": [{
+      "html": "ignore" | "enforce",
+      "custom": "ignore" | "enforce",
+      "explicitSpread": "ignore" | "enforce",
+      
+  }]
   },
 };
