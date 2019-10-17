fc4e2b0f51a8c76685c2b2b242abd9e164dc5fd5
Ruan Aragao <aragao.ruan@gmail.com>
2019-10-17 15:16:25 -0300

    Estilizando SingIn e SingUp

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
-  return <h1>SingUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
