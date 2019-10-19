3a2850c8e84598cba97a30d269e591928bfc9240
Ruan Aragao <aragao.ruan@gmail.com>
2019-10-19 11:46:43 -0300

    autenticacao de usuario.

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+/** Actions */
+import { singInRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -13,8 +17,9 @@ const schema = Yup.object().shape({
 });
 
 export default function SingIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+  function handleSubmit({ email, password }) {
+    dispatch(singInRequest(email, password));
   }
 
   return (
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,26 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import api from '~/services/api';
+import history from '~/services/history';
+import { singInSuccess } from './actions';
 
-export default all([]);
+export function* singIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuario nao e prestador');
+    return;
+  }
+
+  yield put(singInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SING_IN_REQUEST', singIn)]);
