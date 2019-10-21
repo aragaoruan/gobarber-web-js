1592126454347cf688013d8b9a795c68cd9de304
Ruan Aragao <aragao.ruan@gmail.com>
2019-10-21 15:02:48 -0300

    Adicionando toastify

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
@@ -18,6 +18,8 @@ const schema = Yup.object().shape({
 
 export default function SingIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
   function handleSubmit({ email, password }) {
     dispatch(singInRequest(email, password));
   }
@@ -30,7 +32,7 @@ export default function SingIn() {
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
-        <button type="submit">Entrar</button>
+        <button type="submit">{loading ? 'Carregando...' : 'Entrar'}</button>
 
         <Link to="/register">Criar conta gratuita</Link>
       </Form>
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,26 +1,32 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
-import { singInSuccess } from './actions';
+import { singInSuccess, singFailure } from './actions';
 
 export function* singIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuario nao e prestador');
-    return;
+    if (!user.provider) {
+      toast.error('Usuario não é prestador');
+      return;
+    }
+
+    yield put(singInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados.');
+    yield put(singFailure());
   }
-
-  yield put(singInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SING_IN_REQUEST', singIn)]);
