d0f7ac9cdfca2de14296def5e0b01eeb9be8a89c
Ruan Aragao <aragao.ruan@gmail.com>
2019-10-22 14:07:05 -0300

    Header

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Notifications() {
+  return (
+    <Container>
+      <Badge></Badge>
+    </Container>
+  );
+}
