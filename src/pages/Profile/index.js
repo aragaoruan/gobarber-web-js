7e95a65cc21ee0ee53379f3e4f31da5a5129f18b
Ruan Aragao <aragao.ruan@gmail.com>
2019-10-29 11:28:53 -0300

    atualizando perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Form, Input } from '@rocketseat/unform';
+import { updateProfileRequest } from '~/store/modules/user/action';
+
+import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
 
   function handleSubmit(data) {
-    console.tron.log(data);
+    dispatch(updateProfileRequest(data));
   }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
 
@@ -34,7 +40,7 @@ export default function Profile() {
           placeholder="Confirmação de senha"
         />
 
-        <button type="button">Atualizar perfil</button>
+        <button type="submit">Atualizar perfil</button>
       </Form>
       <button type="button">Sair do app</button>
     </Container>
